213a90615f2d09398d752338ff9b893099f3da44
Thomas Kappler <thkapple@microsoft.com>
2017-04-14 07:58:38 -0700

    Simplify text extractor after merge from master.

--- AzureSearchCrawler/MsDocsTextExtractor.cs
+++ AzureSearchCrawler/MsDocsTextExtractor.cs
@@ -1,33 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
+﻿using HtmlAgilityPack;
 
 namespace AzureSearchCrawler
 {
     class MsDocsTextExtractor : TextExtractor
     {
-        private readonly Regex whitespace = new Regex(@"\s+");
-
         public override string ExtractText(HtmlDocument doc)
         {
-            if (doc == null || doc.DocumentNode == null)
-            {
-                return null;
-            }
-
-            RemoveNodesOfType(doc, "script", "style", "svg", "path");
-
-            string content = ExtractTextFromFirstMatchingElement(doc, "//div[@class = 'content']");
-
-            if (content != null)
-            {
-                content = whitespace.Replace(content, " ");
-            }
-
-            return content;
+            return GetCleanedUpTextForXpath(doc, "//div[@class = 'content']");
         }
     }
 }
